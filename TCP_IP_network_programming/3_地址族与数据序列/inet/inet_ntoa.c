787a064e0332d143ca42b30d08baa5835b7cbe34
yz <1126978661@qq.com>
2024-02-20 14:25:05 +0800

    🦊 update docs

--- TCP_IP_network_programming/3_地址族与数据序列/inet/inet_ntoa.c
+++ TCP_IP_network_programming/3_地址族与数据序列/inet/inet_ntoa.c
@@ -8,11 +8,11 @@ int main(int argc, char *argv[])
     char *str_ptr;
     char str_arr[20];
 
-    addr1.sin_addr.s_addr=htonl(0x1020304);
-    addr2.sin_addr.s_addr=htonl(0x1010101);
+    addr1.sin_addr.s_addr = htonl(0x1020304);
+    addr2.sin_addr.s_addr = htonl(0x1010101);
 
     // 向inet_ntoa 函数传递结构体变量addr1中的IP地址信息并调用该函数，返回字符串形式的IP地址
-    str_ptr=inet_ntoa(addr1.sin_addr);
+    str_ptr = inet_ntoa(addr1.sin_addr);
     // 复制addr1中的IP地址
     strcpy(str_arr, str_ptr);
     printf("dotted-decimal notation1: %s\n", str_ptr);
